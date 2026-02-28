e67690eda798653a19278345ec2d13f3b1250d1b
nghiaz <tangnghia47@gmail.com>
2026-02-28 10:36:21 +0700

    be nghia nop lab7

--- bai2.c
+++ bai2.c
@@ -10,3 +10,31 @@
 
 // VIẾT CODE Ở ĐÂY
 
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char username[50];
+    char password[50];
+
+    // Tài khoản mặc định
+    char correctUser[] = "admin";
+    char correctPass[] = "123456";
+
+    printf("Nhap username: ");
+    fgets(username, sizeof(username), stdin);
+    username[strcspn(username, "\n")] = '\0';  // xóa ký tự xuống dòng
+
+    printf("Nhap password: ");
+    fgets(password, sizeof(password), stdin);
+    password[strcspn(password, "\n")] = '\0';  // xóa ký tự xuống dòng
+
+    if(strcmp(username, correctUser) == 0 &&
+       strcmp(password, correctPass) == 0) {
+        printf("Dang nhap thanh cong!\n");
+    } else {
+        printf("Dang nhap khong thanh cong!\n");
+    }
+
+    return 0;
+}
--- bai3.c
+++ bai3.c
@@ -10,3 +10,37 @@
 
 // VIẾT CODE Ở ĐÂY
 
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[5][100];
+    char temp[100];
+    int i, j;
+
+    printf("Nhap 5 chuoi:\n");
+
+    for(i = 0; i < 5; i++) {
+        printf("Chuoi %d: ", i + 1);
+        fgets(str[i], sizeof(str[i]), stdin);
+        str[i][strcspn(str[i], "\n")] = '\0'; // xóa ký tự xuống dòng
+    }
+
+    // Sắp xếp theo thứ tự alphabet
+    for(i = 0; i < 4; i++) {
+        for(j = i + 1; j < 5; j++) {
+            if(strcmp(str[i], str[j]) > 0) {
+                strcpy(temp, str[i]);
+                strcpy(str[i], str[j]);
+                strcpy(str[j], temp);
+            }
+        }
+    }
+
+    printf("\nDanh sach sau khi sap xep:\n");
+    for(i = 0; i < 5; i++) {
+        printf("%s\n", str[i]);
+    }
+
+    return 0;
+}
